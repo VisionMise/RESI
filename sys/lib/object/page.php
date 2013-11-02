3a780edef490d02b007428f30863a4a5726287cf
Vision <geoffrey.kuhl@gmail.com>
2013-11-01 21:56:32 -0700

    Page Handler
    Added page handler to parse page request input in to UI path. UI path
    still required to be added to filesystem.

--- sys/lib/object/page.php
+++ sys/lib/object/page.php
@@ -1,1 +1,17 @@
-<? print "!"; ?>
+<?php
+
+	$request	= array_filter(explode('/', $_GET['page']));
+	$pageInput	= array();
+
+	foreach ($request as $item) {
+		if (item == 'page') continue;
+		$pageInput[] = $item;
+	}
+
+	chdir("../../../");
+	$appStr 	= implode('/', $pageInput);
+	$path 		= getcwd()."/ui/$appStr";
+
+	print_r($path);
+
+?>
